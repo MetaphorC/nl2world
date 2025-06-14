33d7189ab4ab5a9b7b8630fb30538463af8fbf0f
MetaphorC <ypstomar57@gmail.com>
2025-06-14 20:37:15 +0530

    sync with digital ocean and deploy cd pipeline

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,7 +2,6 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::*;
@@ -48,8 +47,7 @@ async fn spawn_app() -> TestApp {
 
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // create database
-    let mut connection = PgConnection::connect(&config.connection_string_without_db()
-        .expose_secret())
+    let mut connection = PgConnection::connect_with(&config.without_db())
         .await
         .expect("Failed to connect to Postgres");
     connection
@@ -58,8 +56,7 @@ pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
         .expect("Failed to create database.");
 
     // migrate database
-    let connection_pool = PgPool::connect(&config.connection_string()
-        .expose_secret())
+    let connection_pool = PgPool::connect_with(config.with_db())
         .await
         .expect("Failed to connect to Postgres.");
     sqlx::migrate!("./migrations")
